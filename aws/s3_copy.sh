e797560d5b87b31570ddff8b7df3170c10a6e682
spriyansh <spriyansh29@gmail.com>
2024-12-12 15:47:32 +0100

    test

--- aws/s3_copy.sh
+++ aws/s3_copy.sh
@@ -1,21 +1,22 @@
 # Create Data
-mkdir -p /home/ec2-user/s3 /home/ec2-user/s3/ASV /home/ec2-user/OTU /home/ec2-user/MicrobialData_DBs
+mkdir -p /home/ec2-user/s3 
+mkdir -p /home/ec2-user/s3/ASV /home/ec2-user/OTU /home/ec2-user/s3/MicrobialData_DBs
 
 # ASVs
-aws s3 cp s3://chernobyl-soil-memory-optim/ASV/dada2-rep-seqs.qza /home/ec2-user/s3/
-aws s3 cp s3://chernobyl-soil-memory-optim/ASV/dada2-table.qza /home/ec2-user/s3/
+aws s3 cp s3://chernobyl-soil-memory-optim/ASV/dada2-rep-seqs.qza /home/ec2-user/s3/ASV/.
+aws s3 cp s3://chernobyl-soil-memory-optim/ASV/dada2-table.qza /home/ec2-user/s3/ASV/.
 
 # OTUs
-aws s3 cp s3://chernobyl-soil-memory-optim/OTU/vcluster-open-ref-rep-seqs.qza /home/ec2-user/s3
-aws s3 cp s3://chernobyl-soil-memory-optim/OTU/vcluster-open-ref-table.qza /home/ec2-user/s3
+aws s3 cp s3://chernobyl-soil-memory-optim/OTU/vcluster-open-ref-rep-seqs.qza /home/ec2-user/s3/OTU/.
+aws s3 cp s3://chernobyl-soil-memory-optim/OTU/vcluster-open-ref-table.qza /home/ec2-user/s3/OTU/.
 
 # Metadata
-aws s3 cp s3://chernobyl-soil-memory-optim/Qiime2MetadataInput.tsv /home/ec2-user/s3/
+aws s3 cp s3://chernobyl-soil-memory-optim/Qiime2MetadataInput.tsv /home/ec2-user/s3/.
 
 # Silva files
-aws s3 cp s3://chernobyl-soil-memory-optim/MicrobialData_DBs/silva-138-99-515-806-classifier.qza /home/ec2-user/s3/
-aws s3 cp s3://chernobyl-soil-memory-optim/MicrobialData_DBs/silva-138-99-seqs-515-806.qza /home/ec2-user/s3/
-aws s3 cp s3://chernobyl-soil-memory-optim/MicrobialData_DBs/taxonomy.tsv /home/ec2-user/s3
+aws s3 cp s3://chernobyl-soil-memory-optim/MicrobialData_DBs/silva-138-99-515-806-classifier.qza /home/ec2-user/s3/MicrobialData_DBs/.
+aws s3 cp s3://chernobyl-soil-memory-optim/MicrobialData_DBs/silva-138-99-seqs-515-806.qza /home/ec2-user/s3/MicrobialData_DBs/.
+aws s3 cp s3://chernobyl-soil-memory-optim/MicrobialData_DBs/taxonomy.tsv /home/ec2-user/s3/MicrobialData_DBs/.
 
 # Clone
 git clone https://github.com/spriyansh/ChernobylNetwork.git
